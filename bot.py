0928e59b568d2e4996600bb64be47e38a9b004dc
Yassin-567 <83673138+Yassin-567@users.noreply.github.com>
2023-07-23 21:04:53 +0300

    gpt bot.py

--- bot.py
+++ bot.py
@@ -1,38 +1,38 @@
-import os
-import asyncio
-import ffmpeg
-from aiogram import Bot, types
-from aiogram.dispatcher import Dispatcher
-from aiogram.utils import executor
-
-# API token from BotFather
-BOT_TOKEN = "6192009745:AAEmB8TpAnPDYroterQfACHIzGT5r3B7p5E"
-
-# Initialize bot and dispatcher
-bot = Bot(token=BOT_TOKEN)
-dp = Dispatcher(bot)
-
-# Handler for receiving videos
-@dp.message_handler(content_types=types.ContentType.VIDEO)
-async def handle_video(message: types.Message):
-    # Get the video file
-    video = message.video
-
-    # Download the video file
-    video_path = f"{video.file_id}.mp4"
-    await bot.download_file_by_id(video.file_id, video_path)
-
-    # Compress the video using ffmpeg
-    compressed_path = f"{video.file_id}_compressed.mp4"
-    ffmpeg.input(video_path).output(compressed_path).run()
-
-    # Send the compressed video back
-    await bot.send_video(message.chat.id, video=open(compressed_path, "rb"))
-
-    # Clean up the files
-    os.remove(video_path)
-    os.remove(compressed_path)
-
-# Start the bot
-if name == 'main':
+import os 
+import asyncio 
+import ffmpeg 
+from aiogram import Bot, types 
+from aiogram.dispatcher import Dispatcher 
+from aiogram.utils import executor 
+  
+# API token from BotFather 
+BOT_TOKEN = "6192009745:AAEmB8TpAnPDYroterQfACHIzGT5r3B7p5E" 
+  
+# Initialize bot and dispatcher 
+bot = Bot(token=BOT_TOKEN) 
+dp = Dispatcher(bot) 
+  
+# Handler for receiving videos 
+@dp.message_handler(content_types=types.ContentType.VIDEO) 
+async def handle_video(message: types.Message): 
+    # Get the video file 
+    video = message.video 
+  
+    # Download the video file 
+    video_path = f"{video.file_id}.mp4" 
+    await bot.download_file_by_id(video.file_id, video_path) 
+  
+    # Compress the video using ffmpeg 
+    compressed_path = f"{video.file_id}_compressed.mp4" 
+    ffmpeg.input(video_path).output(compressed_path).run() 
+  
+    # Send the compressed video back 
+    await bot.send_video(message.chat.id, video=open(compressed_path, "rb")) 
+  
+    # Clean up the files 
+    os.remove(video_path) 
+    os.remove(compressed_path) 
+  
+# Start the bot 
+if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
